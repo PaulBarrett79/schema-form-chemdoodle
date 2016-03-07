a493078389cb4aa8acf2e27bd18edb07911553a7
paulbarrett79 <paulbarrett79@gmail.com>
2016-03-07 17:10:07 +0000

    initial setup of schema form elements

---
+++ schema-form-chemdoodle.js
@@ -0,0 +1,22 @@
+angular.module('schemaForm-chemdoodle', ['schemaForm', 'chemdoodleAngular']).config(
+['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfPathProvider',
+  function(schemaFormProvider,  schemaFormDecoratorsProvider, sfPathProvider) {
+
+    var file_upload = function(name, schema, options) {
+      if (schema.type === 'object' && schema.format == 'file_upload') {
+        var f = schemaFormProvider.stdFormObj(name, schema, options);
+        f.key  = options.path;
+        f.type = 'file_upload';
+        options.lookup[sfPathProvider.stringify(options.path)] = f;
+        return f;
+      }
+    };
+
+    schemaFormProvider.defaults.object.unshift(file_upload);
+
+  //Add to the bootstrap directive
+    schemaFormDecoratorsProvider.addMapping('bootstrapDecorator', 'chemdoodle',
+    'directives/decorators/bootstrap/chemdoodle/chemdoodle-form-template.html');
+    schemaFormDecoratorsProvider.createDirective('chemdoodle',
+    'directives/decorators/bootstrap/chemdoodle/chemdoodle-form-template.html');
+  }]);
